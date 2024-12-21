f7bb8ffbdf7a16a509c487072da96e7d8ab300da
李力士 <lilishi@chinasofti.com>
2024-12-21 16:13:32 +0800

    鑷姩鎻愪氦鐢熸垚鐨勪唬鐮?

--- main.bat
+++ main.bat
@@ -1,3 +1,6 @@
 @echo off
-go run main.go -tll 40 -tl 300 -httping -cfcolo HKG,NRT,ICN,DXB,SIN,NYC,LAX,SFO,KIX,BOM,DEL,JKT,KUL,TPE -dd
+go run main.go -p 0 -tll 40 -tl 300 -httping -cfcolo HKG,NRT,ICN,DXB,SIN,NYC,LAX,SFO,KIX,BOM,DEL,JKT,KUL,TPE -dd
+git add .
+git commit -m "自动提交生成的代码"
+git push
 pause
